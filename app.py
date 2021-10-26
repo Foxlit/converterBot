f6a2812b6fe520143d7156cbcd90739e45a1915c
Foxlit <v.gromov2006@gmail.com>
2021-10-26 22:24:47 +0300

    add currencies

--- app.py
+++ app.py
@@ -16,13 +16,37 @@ def open_config_file():  # Получение настроек бота
     return bot_config_file
 
 
+def open_currencies_file():  # Получение настроек бота
+    if os.path.exists('currencies.json'):
+        with open('currencies.json', 'r') as currencies_file:
+            currencies_file = json.load(currencies_file)
+    else:  # если файл настроек не найден
+        empty_currencies = {'Bitcoin': "BTC", 'Ruble': "RUB", 'Dollar': 'USD'}
+        with open('currencies.json', 'w') as empty_currencies_file:
+            # Создадим пустой файл настройки
+            empty_currencies_file.write(json.dumps(empty_currencies))
+            print("Нет файла настройки. Создан пустой файл настройки в текущем каталоге")
+    return currencies_file
+
+
+currencies_list = open_currencies_file()
 bot_config = open_config_file()
 bot = telebot.TeleBot(bot_config["token"])
-h=7
 
-@bot.message_handler()
-def echo_test(messege: telebot.types.Message):
-    bot.send_message(messege.chat.id, "hello")
+
+@bot.message_handler(commands=['start'])
+def start(message: telebot.types.Message):
+    text = 'Чтобы начать работу с ботом введите следующие данные:\n Валюта из которой предстоит конвертировать\n ' \
+        'Валюта в которую предстоит конвертировать\n Количество конвертируемой валюты'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['currencies'])
+def currencies(message: telebot.types.Message):
+    text = "Доступные валюты: "
+    for currency in currencies_list:
+        text = '\n'.join((text, currency, ))
+    bot.reply_to(message, text)
 
 
 bot.polling()
